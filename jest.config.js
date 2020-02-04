c339ff7d03761620f7a3b1543b7a317d16e44b54
Jasper <jasper@vanderstoop.nl>
2020-02-04 17:52:52 +0100

    :wrench: add Jest config file

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+/* eslint-disable max-len */
+// For a detailed explanation regarding each configuration property, visit:
+// https://jestjs.io/docs/en/configuration.html
+
+module.exports = {
+  transformIgnorePatterns: [
+    "/node_modules/",
+    "/src/",
+  ],
+};
